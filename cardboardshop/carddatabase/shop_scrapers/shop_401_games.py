a082192cad2b037b3f8bdbd6a7c27d08559f5959
liangyo8 <yonlin.liang@mail.utoronto.ca>
2023-02-24 11:58:57 -0800

    refactored and cleaned up 401 scrape code to now inclue rarity and set information

---
+++ cardboardshop/carddatabase/shop_scrapers/shop_401_games.py
@@ -0,0 +1,68 @@
+import threading
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def card_prices_401_helper(card_name, card_sets, card_edition=None):
+    urls = []
+    set_informations = []
+    for sets in card_sets:
+        set_informations.append(sets['set_code'].replace(' ', '-') + ' ' + sets['set_rarity'].replace(' ', '-'))
+        urls.append("https://store.401games.ca/products/" + card_name.replace(' ', '-') \
+                    + "-" + sets['set_code'].replace(' ', '-') + "-" + sets['set_rarity'].replace(' ',
+                                                                                                  '-') + "-" + "1st-edition")
+
+    print(urls)
+
+    results = []
+
+    def worker_data(url, set_information):
+        result = get_data(url, set_information)
+        results.append(result)
+
+    # implement multithreading
+    threads = []
+    i = 0
+    for url in urls:
+        t = threading.Thread(target=worker_data, args=(url, set_informations[i]))
+        threads.append(t)
+        t.start()
+        i += 1
+
+    for t in threads:
+        t.join()
+
+    print(results)
+    return results
+
+
+def get_data(url, set_information):
+    try:
+        response = requests.get(url)
+        html = response.content
+
+        # Create a BeautifulSoup object
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Extract the title of the webpage
+        title = soup.title.string
+        print("Title:", title)
+
+        # Extract all the links in the page
+        cost = soup.find(id="ProductPrice-product-template").text.strip()
+        stock_status = soup.find(id="AddToCartText-product-template").text.strip()
+        rarity = set_information
+        if stock_status == 'Add to Cart':
+            stock_status = 'In Stock'
+        else:
+            stock_status = 'Out of Stock'
+
+        return [cost, stock_status, rarity]
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 404:
+            print('404 error: page not found')
+        else:
+            print('HTTP error:', e)
+    else:
+        print(response.content)
--- cardboardshop/carddatabase/views.py
+++ cardboardshop/carddatabase/views.py
@@ -1,14 +1,10 @@
-import threading
-
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import get_object_or_404, render
-from django.utils import timezone
 from datetime import date
 
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+
 from .models import CardInfo
-from bs4 import BeautifulSoup
-import requests
-import random
+from .shop_scrapers.shop_401_games import card_prices_401_helper
 
 
 def index(request):
@@ -28,65 +24,7 @@ def update_records(request):
         card_id = request.POST.get('card_id')
         card = CardInfo.objects.get(id=card_id)
         card.updated_date = date.today()
-        card.card_prices[0]['401_games'] = _card_prices_401_helper(card.name, card.card_sets)
+        card.card_prices[0]['401_games'] = card_prices_401_helper(card.name, card.card_sets)
         card.save()
         data = {'message': 'Last updated date has been updated.'}
         return JsonResponse(data)
-
-
-def _card_prices_401_helper(card_name, card_sets, card_edition=None):
-    url = "https://store.401games.ca/products/pot-of-prosperity-blvo-en065-secret-rare-1st-edition"
-    urls = []
-    for sets in card_sets:
-        urls.append("https://store.401games.ca/products/" + card_name.replace(' ', '-') \
-                    + "-" + sets['set_code'].replace(' ', '-') + "-" + sets['set_rarity'].replace(' ',
-                                                                                                  '-') + "-" + "1st-edition")
-
-    print(urls)
-
-    results = []
-
-    def worker_data(url):
-        result = get_data(url)
-        results.append(result)
-
-    # implement multithreading
-    threads = []
-    for url in urls:
-        t = threading.Thread(target=worker_data, args=(url,))
-
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    print(results)
-    return results
-
-
-def get_data(url):
-    try:
-        response = requests.get(url)
-        html = response.content
-
-        # Create a BeautifulSoup object
-        soup = BeautifulSoup(html, 'html.parser')
-
-        # Extract the title of the webpage
-        title = soup.title.string
-        print("Title:", title)
-
-        # Extract all the links in the page
-        cost = soup.find(id="ProductPrice-product-template")
-        print(cost.text.strip())
-        return cost.text.strip()
-    except requests.exceptions.HTTPError as e:
-        if e.response.status_code == 404:
-            print('404 error: page not found')
-        else:
-            print('HTTP error:', e)
-    else:
-        print(response.content)
-
-
