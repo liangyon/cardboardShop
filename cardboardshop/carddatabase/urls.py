c844f5a5e0cf2efb9fe13025f8fc3d2399bdb529
liangyo8 <yonlin.liang@mail.utoronto.ca>
2023-02-23 23:27:56 -0800

    completed data scraping basis for 401games,
    next I need to include rarity and set information for the prices, as well as stock info
    
    then, need to add other stores such as A&C, cardbrawlers, and etc.

--- cardboardshop/carddatabase/urls.py
+++ cardboardshop/carddatabase/urls.py
@@ -5,10 +5,6 @@ from .views import card_detail
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('card/<int:card_id>', views.card_detail, name='card_detail')
+    path('card/<int:card_id>', views.card_detail, name='card_detail'),
+    path('update_records/', views.update_records, name='update_records'),
 ]
--- cardboardshop/carddatabase/views.py
+++ cardboardshop/carddatabase/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse
+import threading
+
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
+from datetime import date
+
 from .models import CardInfo
+from bs4 import BeautifulSoup
+import requests
 import random
 
 
@@ -11,19 +18,75 @@ def index(request):
     return render(request, 'carddatabase/home.html', context)
 
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
 def card_detail(request, card_id):
     card = get_object_or_404(CardInfo, id=card_id)
     return render(request, 'carddatabase/card_detail.html', {'card': card})
+
+
+def update_records(request):
+    if request.method == 'POST':
+        card_id = request.POST.get('card_id')
+        card = CardInfo.objects.get(id=card_id)
+        card.updated_date = date.today()
+        card.card_prices[0]['401_games'] = _card_prices_401_helper(card.name, card.card_sets)
+        card.save()
+        data = {'message': 'Last updated date has been updated.'}
+        return JsonResponse(data)
+
+
+def _card_prices_401_helper(card_name, card_sets, card_edition=None):
+    url = "https://store.401games.ca/products/pot-of-prosperity-blvo-en065-secret-rare-1st-edition"
+    urls = []
+    for sets in card_sets:
+        urls.append("https://store.401games.ca/products/" + card_name.replace(' ', '-') \
+                    + "-" + sets['set_code'].replace(' ', '-') + "-" + sets['set_rarity'].replace(' ',
+                                                                                                  '-') + "-" + "1st-edition")
+
+    print(urls)
+
+    results = []
+
+    def worker_data(url):
+        result = get_data(url)
+        results.append(result)
+
+    # implement multithreading
+    threads = []
+    for url in urls:
+        t = threading.Thread(target=worker_data, args=(url,))
+
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    print(results)
+    return results
+
+
+def get_data(url):
+    try:
+        response = requests.get(url)
+        html = response.content
+
+        # Create a BeautifulSoup object
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Extract the title of the webpage
+        title = soup.title.string
+        print("Title:", title)
+
+        # Extract all the links in the page
+        cost = soup.find(id="ProductPrice-product-template")
+        print(cost.text.strip())
+        return cost.text.strip()
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 404:
+            print('404 error: page not found')
+        else:
+            print('HTTP error:', e)
+    else:
+        print(response.content)
+
+
